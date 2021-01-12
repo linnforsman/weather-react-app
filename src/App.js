533794594f44d8bfaba49d2ff4ecb67cd2468d01
Linn Forsman <linnmetal7@gmail.com>
2021-01-12 14:13:50 +0100

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"><h1>Weather App</h1>
+    <div className="App">
+      <div className="container">
+      <h1>Weather App</h1>
+
       <footer>
         This project is coded by <a href="https://www.linkedin.com/in/linnforsman/" target="_blank"> Linn Forsman </a>and is{" "}
           <a href="https://github.com/linnforsman/weather-react-app" target="_blank"
         > open-source</a>
       </footer>
+      </div>
     </div>
   );
 }
