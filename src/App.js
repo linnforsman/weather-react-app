227908247219d7c64f6b0a8da4e4269398be4fb1
Linn Forsman <linnmetal7@gmail.com>
2021-01-12 14:00:43 +0100

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App"><h1>Weather App</h1>
+      <footer>
+        This project is coded by <a href="https://www.linkedin.com/in/linnforsman/" target="_blank"> Linn Forsman </a>and is{" "}
+          <a href="https://github.com/linnforsman/weather-react-app" target="_blank"
+        > open-source</a>
+      </footer>
+    </div>
   );
 }
