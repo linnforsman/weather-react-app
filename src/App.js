a9c8fa0ac05c6af26700088930847575d1f8b88b
Linn Forsman <linnmetal7@gmail.com>
2021-02-05 14:33:01 +0100

    Minor fix URL
    Forgot colon in API call and some deletion of unnecessary files

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
       <div className="container">
       <Weather defaultCity="Oslo"/>
       <footer>
-        This project is coded by <a href="https://www.linkedin.com/in/linnforsman/" target="_blank"> Linn Forsman </a>and is{" "}
-          <a href="https://github.com/linnforsman/weather-react-app" target="_blank" 
+        This project is coded by <a href="https://www.linkedin.com/in/linnforsman/" rel="noreferrer" target="_blank"> Linn Forsman </a>and is{" "}
+          <a href="https://github.com/linnforsman/weather-react-app" rel="noreferrer" target="_blank" 
         > open-source</a> on Github.
       </footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 
 export default function Weather(props) {
 
-    const [weatherData, setWeatherData] = useState({ready:false});
-
+    const [weatherData, setWeatherData] = useState({ ready: false });
     function handleResponse(response) {
       console.log(response.data);
       setWeatherData({
          ready: true,
          temperature: response.data.main.temp,
          humidity: response.data.main.humidity,
-         date: "Tuesday 14:00",
+         date: new Date(response.data.dt * 1000),
          description: response.data.weather[0].description,
          iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
          wind: response.data.wind.speed,
-         city: response.data.name
+         city: response.data.name,
       });
   }
 
@@ -36,7 +36,7 @@ export default function Weather(props) {
     </form>
     <h1>{weatherData.city}</h1>
     <ul>
-        <li>{weatherData.date}</li>
+        <li><FormattedDate date={weatherData.date} /></li>
         <li className="text-capitalize">{weatherData.description}</li>
     </ul>    
      <div className="row mt-3">
@@ -60,11 +60,11 @@ export default function Weather(props) {
         </div>   
    </div>
    );
-  } else {
+   } else {
     const apiKey = "a18c8104bb6bfa9db3920bc0a74cc868";
-    let apiUrl = `http//api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-
+    
     return "Loading...";
-  }    
+  }   
 }
