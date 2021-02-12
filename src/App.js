55e81f40f08deaea911203529b012dfe6dc0ba6b
Linn Forsman <linnmetal7@gmail.com>
2021-02-12 13:01:33 +0100

    Added minor info in the footer
    - link to netlify

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from "react";
 import Weather from "./Weather.js";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather defaultCity="Oslo"/>
-      <footer>
-        This project is coded by <a href="https://www.linkedin.com/in/linnforsman/" rel="noreferrer" target="_blank"> Linn Forsman </a>and is{" "}
-          <a href="https://github.com/linnforsman/weather-react-app" rel="noreferrer" target="_blank" 
-        > open-source</a> on Github.
-      </footer>
+        <Weather defaultCity="Oslo" />
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/linnforsman/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {" "}
+            Linn Forsman{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/linnforsman/weather-react-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {" "}
+            open-source
+          </a>{" "}
+          on Github, and hosted on{" "}
+          <a
+            href="https://competent-aryabhata-aaf80f.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
       </div>
     </div>
   );
